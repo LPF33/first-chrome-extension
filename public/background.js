1df32b74c78c43027330b3952b1dc1bf06b20f21
LPF33 <larspefe@gmail.com>
2021-08-08 01:22:14 +0200

    feat(ColorPicker): get rgb value for color under mouse in website

--- public/background.js
+++ public/background.js
@@ -1,20 +1,97 @@
-chrome.commands.onCommand.addListener((command) => {
-    console.log("command ", command);
+chrome.commands.onCommand.addListener(async (command) => {
+    console.log(command);
+    const tabs = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+    });
+    if (!tabs.length) {
+        return;
+    }
     if (command === "save-snippet") {
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            chrome.scripting.executeScript({
-                target: {
-                    tabId: tabs[0].id,
-                },
-                function: function () {
-                    const selection = `${window.getSelection()}`;
-                    chrome.runtime.sendMessage({
-                        type: "save-selected-text",
-                        selection: selection.trim(),
-                        href: window.location.href,
+        chrome.scripting.executeScript({
+            target: {
+                tabId: tabs[0].id,
+            },
+            function: function () {
+                const selection = `${window.getSelection()}`;
+                chrome.runtime.sendMessage({
+                    type: "save-selected-text",
+                    selection: selection.trim(),
+                    href: window.location.href,
+                });
+            },
+        });
+    }
+
+    if (command === "pick-color") {
+        chrome.scripting.executeScript({
+            target: {
+                tabId: tabs[0].id,
+            },
+            function: async function () {
+                const canvas = document.createElement("canvas");
+                const ctx = canvas.getContext("2d");
+                const videoElem = document.createElement("video");
+
+                try {
+                    const captureStream =
+                        await navigator.mediaDevices.getDisplayMedia();
+                    let { width, height } = captureStream
+                        .getTracks()[0]
+                        .getSettings();
+                    canvas.width = width;
+                    canvas.height = height;
+                    videoElem.autoplay = true;
+                    videoElem.srcObject = captureStream;
+                    videoElem.addEventListener("loadeddata", () => {
+                        if (videoElem.readyState === 4) {
+                            setTimeout(getScreenShot, 200);
+                        }
                     });
-                },
-            });
+                    function getScreenShot() {
+                        ctx.drawImage(videoElem, 0, 0, width, height);
+                        captureStream
+                            .getTracks()
+                            .forEach((track) => track.stop());
+
+                        document.body.addEventListener("mousedown", (e) =>
+                            getRGBValue(e, ctx)
+                        );
+                        function getRGBValue(e, ctx) {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            const imageData = ctx.getImageData(
+                                0,
+                                0,
+                                width,
+                                height
+                            );
+                            const rgb = { r: 0, g: 0, b: 0 };
+                            function getColorIndicesForCoord(x, y, width) {
+                                var red = y * (width * 4) + x * 4;
+                                return [red, red + 1, red + 2, red + 3];
+                            }
+                            const [red, green, blue] = getColorIndicesForCoord(
+                                e.x,
+                                e.y +
+                                    window.screen.availHeight -
+                                    window.innerHeight,
+                                width
+                            );
+                            rgb.r = imageData.data[red];
+                            rgb.g = imageData.data[green];
+                            rgb.b = imageData.data[blue];
+                            chrome.storage.sync.set({ rgb });
+                            document.body.removeEventListener(
+                                "mousedown",
+                                (e) => getRGBValue(e, ctx)
+                            );
+                        }
+                    }
+                } catch (err) {
+                    console.error("Error: " + err);
+                }
+            },
         });
     }
 });
--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "../styles/ColorPicker.css";
 
 export default function ColorPicker() {
-    return <div>ColorPicker</div>;
+    const [rgbValue, setRGBValue] = useState(null);
+    const [hex, setHex] = useState(null);
+
+    useEffect(() => {
+        chrome.storage.sync.get(["rgb"], ({ rgb }) => {
+            setRGBValue(rgb);
+            const { r, g, b } = rgb;
+            setHex(
+                `#${r.toString(16).padStart(2, "0")}${g
+                    .toString(16)
+                    .padStart(2, "0")}${b.toString(16).padStart(2, "0")}`
+            );
+        });
+    }, []);
+
+    const selectColor = (e) => {
+        const textarea = document.createElement("textarea");
+        textarea.textContent = e.target.innerText;
+        document.body.appendChild(textarea);
+        textarea.select();
+        document.execCommand("copy");
+        console.dir(textarea);
+        textarea.remove();
+    };
+
+    if (!rgbValue) {
+        return (
+            <div className="color-picker">
+                <h1>ColorPicker</h1>
+                <p>No color picked</p>
+            </div>
+        );
+    }
+
+    return (
+        <div className="color-picker">
+            <h1>ColorPicker</h1>
+            <div
+                id="preview"
+                style={{
+                    backgroundColor: `rgb(${rgbValue.r},${rgbValue.g},${rgbValue.b})`,
+                }}
+            ></div>
+            <p
+                onClick={selectColor}
+            >{`rgb(${rgbValue.r},${rgbValue.g},${rgbValue.b})`}</p>
+            <p onClick={selectColor}>{hex}</p>
+        </div>
+    );
 }
